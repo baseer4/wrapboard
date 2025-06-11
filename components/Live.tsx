f734e7ba015251a679df83c3d7bc63336b8c344c
baseer4 <sbaseer003@gmail.com>
2025-06-11 17:11:57 +0530

    Add cursor chat support within bubble

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,18 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import LiveCursor from './cursor/LiveCursor';
 import { useMyPresence, useOthers } from '@liveblocks/react/suspense';
+import CursorChat from './cursor/CursorChat';
+import { CursorMode } from '@/types/type';
 
 const Live = () => {
     const others = useOthers();
     const [{cursor},updateMyPresence] = useMyPresence();
+
+    const [cursorState, setCursorState] = useState(
+      {
+        mode:CursorMode.Hidden,
+      }
+    )
 
     const handlePointerMove = useCallback((event:React.PointerEvent)=>
       {
@@ -15,24 +23,52 @@ const Live = () => {
         const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
         updateMyPresence({cursor : {x,y}});
-      },[])
+    },[])
     
-     const handlePointerLeave = useCallback((event:React.PointerEvent)=>
+    const handlePointerLeave = useCallback(()=>
       {
-        event.preventDefault();
+        setCursorState({
+          mode: CursorMode.Hidden
+        });
 
         updateMyPresence({cursor :null});
-      },[])
+    },[])
 
-       const handlePointerDown = useCallback((event:React.PointerEvent)=>
+    const handlePointerDown = useCallback((event:React.PointerEvent)=>
       {
         // getBoundingClientRect().x gives you the horizontal position of the element’s left edge relative to the browser window (viewport).
         const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
         const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
         updateMyPresence({cursor : {x,y}});
-      },[])
+    },[])
 
+    useEffect(()=> {
+      const onKeyUp = (e: KeyboardEvent) =>{
+        if(e.key ==='/'){
+          setCursorState({
+            mode:CursorMode.Chat,
+            previousMessage:null,
+            message:'',
+          })
+        }else if(e.key ==='Escape'){
+          updateMyPresence({message:''})
+          setCursorState({mode:CursorMode.Hidden})
+        }
+      }
+
+      const onKeyDown = (e:KeyboardEvent) =>{
+        if(e.key ==='/'){
+          e.preventDefault();
+        }
+      }
+      window.addEventListener('keyup',onKeyUp)
+      window.addEventListener('keydown',onKeyDown)
+      return ()=>{
+        window.removeEventListener('keyup',onKeyUp)
+        window.removeEventListener('keydown',onKeyDown)
+      }
+    },[updateMyPresence])
 
 
 
@@ -47,6 +83,15 @@ const Live = () => {
       <h1 className="text-4xl text-white">
         liveblocks ? working
       </h1>
+        {cursor && (
+          <CursorChat
+            cursor ={cursor}
+            cursorState={cursorState}
+            setCursorState={setCursorState}
+            updateMyPresence={updateMyPresence}
+          />
+        )}
+
         <LiveCursor others ={others}/>
     </div>
   )
