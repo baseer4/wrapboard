f1223ffd9d2f851e5e4570d38bd8c4474c2b0877
baseer4 <sbaseer003@gmail.com>
2025-07-10 00:56:52 +0530

    Update configs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
+import { config } from "process";
 
 const nextConfig: NextConfig = {
+   // webpack:(config) =>{
+   // config.external.push({"utf-8-validate": "commonjs utf-8-validate",
+   // "bufferutil": "commonjs bufferutil",
+   // canvas:"commonjs canvas"
+   //    })
+   // return config;
+   // },
   images:{
     remotePatterns:
     [
@@ -10,7 +18,10 @@ const nextConfig: NextConfig = {
       port:''
        }
    ]
-}
+  },
+   typescript :{
+      ignoreBuildErrors:true
+   }
    };
 
 export default nextConfig;
