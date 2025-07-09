ef32a102bc434f484e9cc0dc86f1e112395f6695
baseer4 <sbaseer003@gmail.com>
2025-07-10 00:06:34 +0530

    Add jspdf for exporting and saving canvas locally

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-// import jsPDF from "jspdf";
+import jsPDF from "jspdf";
 import { twMerge } from "tailwind-merge";
 import { type ClassValue, clsx } from "clsx";
 
@@ -103,19 +103,15 @@ export const exportToPdf = () => {
 
   if (!canvas) return;
 
-  // // use jspdf
-  // const doc = new jsPDF({
-  //   orientation: "landscape",
-  //   unit: "px",
-  //   format: [canvas.width, canvas.height],
-  // });
+  const doc = new jsPDF({
+    orientation: "landscape",
+    unit: "px",
+    format: [canvas.width, canvas.height],
+  });
 
-  // get the canvas data url
   const data = canvas.toDataURL();
 
-  // add the image to the pdf
   doc.addImage(data, "PNG", 0, 0, canvas.width, canvas.height);
 
-  // download the pdf
   doc.save("canvas.pdf");
 };
