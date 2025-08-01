d1e0e5f6f0f1ddc58e2a81a60a3e2585a26a8d50
baseer4 <sbaseer003@gmail.com>
2025-08-01 17:51:24 +0530

    Add favicon and logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,45 +16,50 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
     (Array.isArray(value) && value.some((val) => val?.value === activeElement?.value));
 
   return (
-    <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
-      <Image src="/assets/logo.svg" alt="Logo" width={58} height={20} />
+   <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
+  <div className="flex items-center">
+  <Image src="/assets/logo.png" alt="Logo" width={64} height={80} />
+  <h1 className="-ml-2.5 text-xl font-semibold leading-tight">Wrapboard</h1>
+</div>
 
-      <ul className="flex flex-row">
-        {navElements.map((item: ActiveElement | any) => (
-          <li
-            key={item.name}
-            onClick={() => {
-              if (Array.isArray(item.value)) return;
-              handleActiveElement(item);
-            }}
-            className={`group px-2.5 py-5 flex justify-center items-center
-            ${isActive(item.value) ? "" : "hover:bg-primary-grey-200"}
-            `}
-          >
-            {Array.isArray(item.value) ? (
-              <ShapesMenu
-                item={item}
-                activeElement={activeElement}
-                imageInputRef={imageInputRef}
-                handleActiveElement={handleActiveElement}
-                handleImageUpload={handleImageUpload}
-              />
-            ) : (
-              <Button className="relative w-5 h-5 object-contain">
-                <Image
-                  src={item.icon}
-                  alt={item.name}
-                  fill
-                  className={isActive(item.value) ? "invert" : ""}
-                />
-              </Button>
-            )}
-          </li>
-        ))}
-      </ul>
 
-      <ActiveUsers />
-    </nav>
+  <ul className="flex flex-row">
+    {navElements.map((item: ActiveElement | any) => (
+      <li
+        key={item.name}
+        onClick={() => {
+          if (Array.isArray(item.value)) return;
+          handleActiveElement(item);
+        }}
+        className={`group px-2.5 py-5 flex justify-center items-center
+          ${isActive(item.value) ? "" : "hover:bg-primary-grey-200"}
+        `}
+      >
+        {Array.isArray(item.value) ? (
+          <ShapesMenu
+            item={item}
+            activeElement={activeElement}
+            imageInputRef={imageInputRef}
+            handleActiveElement={handleActiveElement}
+            handleImageUpload={handleImageUpload}
+          />
+        ) : (
+          <Button className="relative w-5 h-5 object-contain">
+            <Image
+              src={item.icon}
+              alt={item.name}
+              fill
+              className={isActive(item.value) ? "invert" : ""}
+            />
+          </Button>
+        )}
+      </li>
+    ))}
+  </ul>
+
+  <ActiveUsers />
+</nav>
+
   );
 };
 
